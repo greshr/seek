4d5cfdcdec292d2548b74c9db19805cae08bb1f3
Matt Dear <mrmdear@gmail.com>
2020-02-08 22:20:44 +0000

    I have now managed to fix the compass literally.

--- Compass.js
+++ Compass.js
@@ -1,1 +1,8 @@
-gh
+import Mathjs;
+bearing(rlat, rlon, llat, llon){
+  var y = Math.sin(rlon-llon) * Math.cos(rlat);
+  var x = Math.cos(llat)*Math.sin(rlat) -
+          Math.sin(llat)*Math.cos(rlat)*Math.cos(rlon-llon);
+  var brng = Math.atan2(y, x).toDegrees();
+  return brng;
+}
