9b4c769ec604c21e59f50383abda905d0f566097
Matt Dear <mrmdear@gmail.com>
2020-02-09 01:20:16 +0000

    demo is now complete.

--- Compass.js
+++
@@ -1,8 +0,0 @@
-import Mathjs;
-bearing(rlat, rlon, llat, llon){
-  var y = Math.sin(rlon-llon) * Math.cos(rlat);
-  var x = Math.cos(llat)*Math.sin(rlat) -
-          Math.sin(llat)*Math.cos(rlat)*Math.cos(rlon-llon);
-  var brng = Math.atan2(y, x).toDegrees();
-  return brng;
-}
